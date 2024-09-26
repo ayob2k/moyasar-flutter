fc6eed83dd01d1f768f019c523ed51a66885610d
Ayob's Macbook Pro <qwer@192.168.1.48>
2024-09-26 19:24:09 +0100

    feat: fix getcardtype mada

--- lib/src/utils/card_utils.dart
+++ lib/src/utils/card_utils.dart
@@ -84,22 +84,29 @@ class CardUtils {
     return CardCompany.visa;
   }
 
-  static String getCardType(String input) {
-    if (input.startsWith(RegExp(
-        r'((5[1-5])|(222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720))'))) {
-      return 'mastercard';
-    } else if (input.startsWith(RegExp(r'((34)|(37))'))) {
-      return 'amex';
-    } else if (input
-        .startsWith(RegExp(r'((4[0-9]{12}(?:[0-9]{3})?)|(4[0-9]{6,}$))'))) {
-      if (input.startsWith(RegExp(r'((50)|(56)|(57)|(58)|(6[0-9]))'))) {
-        return 'mada';
-      }
+  static String getCardType(String cardNumber) {
+    // Remove all spaces and hyphens
+    cardNumber = cardNumber.replaceAll(RegExp(r'[\s-]+'), '');
+
+    // Regular expressions for card types
+    final RegExp visaRegExp = RegExp(r'^4[0-9]{12}(?:[0-9]{3})?$');
+    final RegExp masterCardRegExp = RegExp(r'^(?:5[1-5][0-9]{14})$');
+    final RegExp amexRegExp = RegExp(r'^3[47][0-9]{13}$');
+    final RegExp madaRegExp = RegExp(
+        r'^4(0(0861|1757|7(197|395)|9201)|1(0685|7633|9593)|2(281(7|8|9)|8(331|67(1|2|3)))|3(1361|2328|4107|9954)|4(0(533|647|795)|5564|6(393|404|672))|5(5(036|708)|7865|8456)|6(2220|854(0|1|2|3))|8(301(0|1|2)|4783|609(4|5|6)|931(7|8|9))|93428)|5(0(4300|8160)|13213|2(1076|4(130|514)|9(415|741))|3(0906|1095|2013|5(825|989)|6023|7767|9931)|4(3(085|357)|9760)|5(4180|7606|8848)|8(5265|8(8(4(5|6|7|8|9)|5(0|1))|98(2|3))|9(005|206)))|6(0(4906|5141)|36120)|9682(0(1|2|3|4|5|6|7|8|9)|1(0|1))');
+
+    // Check card type based on regex matching
+    if (visaRegExp.hasMatch(cardNumber)) {
       return 'visa';
-    } else if (input.startsWith(RegExp(r'((50)|(56)|(57)|(58)|(6[0-9]))'))) {
+    } else if (masterCardRegExp.hasMatch(cardNumber)) {
+      return 'mastercard';
+    } else if (amexRegExp.hasMatch(cardNumber)) {
+      return 'amex';
+    } else if (madaRegExp.hasMatch(cardNumber)) {
       return 'mada';
+    } else {
+      return 'unknown';
     }
-    return 'unknown';
   }
 }
 
